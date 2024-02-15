708eecc1a130266d5b7ddcd5f6f0f9c9634d680d
Charlie Silver <cs1108@exeter.ac.uk>
2024-02-15 14:48:50 +0000

    testpull

--- ScrumBuddies/test.py
+++ ScrumBuddies/test.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse
 
 def index(request, question_id):
     question_id = question_id + 1
-    # fajel;fje;l
     question_id = question_id - 123
     return HttpResponse(f"Hello, world. You're at the polls index. {question_id}")
-
-    # sldakfjsaldktjealtij
