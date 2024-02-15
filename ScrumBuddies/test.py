831bf5ddfbe46e1c72d15ccf4ae8ec8fea79b1ee
Charlie Silver <cs1108@exeter.ac.uk>
2024-02-15 14:50:11 +0000

    testpull2

--- ScrumBuddies/test.py
+++ ScrumBuddies/test.py
@@ -5,3 +5,4 @@ def index(request, question_id):
     question_id = question_id + 1
     question_id = question_id - 123
     return HttpResponse(f"Hello, world. You're at the polls index. {question_id}")
+    #test
