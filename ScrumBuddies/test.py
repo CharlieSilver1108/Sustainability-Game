25ed7d9dce6da5677cace1f7d83efcd3395881b4
Luke <lh880@exeter.ac.uk>
2024-02-15 15:13:25 +0000

    test

--- ScrumBuddies/test.py
+++ ScrumBuddies/test.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def index(request, question_id):
     question_id = question_id + 1
     question_id = question_id - 123
-    # cool function
+    # cool functio
     return HttpResponse(f"Hello, world. You're at the polls index. {question_id}")
     #testing
