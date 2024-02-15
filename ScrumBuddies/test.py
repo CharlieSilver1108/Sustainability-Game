49dfa3f0ad368203b0342f941c44f570e7dfbbb8
liam buchanan <=>
2024-02-15 14:34:24 +0000

    Add increment to question_id in index function

--- ScrumBuddies/test.py
+++ ScrumBuddies/test.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 def index(request, question_id):
     question_id = question_id + 1
+    
     return HttpResponse(f"Hello, world. You're at the polls index. {question_id}")
