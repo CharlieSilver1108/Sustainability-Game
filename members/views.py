1ce45f732879df8c3b99b2b6c158e4e8a6285211
Charlie Silver <cs1108@exeter.ac.uk>
2024-02-19 22:39:42 +0000

    Login Function

--- members/views.py
+++ members/views.py
@@ -6,4 +6,18 @@ from django.contrib import messages
 # Create your views here.
 
 def login_user(request):
-    return render(request, 'members/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
+        else:
+            messages.success(request, ("Username or Password Incorrect, Please Try Again"))
+            return redirect('login_user')
+
+    else:
+        return render(request, 'members/login.html', {})
