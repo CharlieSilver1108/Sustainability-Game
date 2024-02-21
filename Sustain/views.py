964f04dd6ff30d8056b2028d2bcada0dc981a00e
Charlie Silver <cs1108@exeter.ac.uk>
2024-02-21 21:02:14 +0000

    Moved 'learning' from members application to Sustain application

--- Sustain/views.py
+++ Sustain/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from html.parser import HTMLParser
+
 
 # Create your views here.
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.data = []
 
-from django.http import HttpResponse
+    def handle_data(self, data):
+        self.data.append(data)
+
 
 
 def index(request):
     return render(request, 'Sustain/index.html', {})
+
+
+
+def learning(request):
+    return render(request, 'Sustain/learning.html', {})
