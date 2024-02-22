0aef101468b832b7e0ec36efb47919d9dcd711ac
Luke <lh880@exeter.ac.uk>
2024-02-22 12:03:01 +0000

    added a table that shows all cuurent tasks and available tasks - still not finished

--- members/models.py
+++ members/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from tasks.models import Task, Task_Type
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    taskOne = models.ForeignKey(Task, related_name='taskOne', on_delete=models.CASCADE, null=True, blank=True)
+    taskTwo = models.ForeignKey(Task, related_name='taskTwo', on_delete=models.CASCADE, null=True, blank=True)
+    taskThree = models.ForeignKey(Task, related_name='taskThree', on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return str(self.user)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Task
 from .models import Task_Type
 
 def task_view(request):
-    data = Task.objects.all()
-    return render(request, 'tasks/tasks.html', {'data': data})
+    allTasks = Task.objects.all()
+    user = request.user
+    profile = user.profile
+
+    currentTaskIDs=[]
+
+    if profile.taskOne:
+        currentTaskIDs.append(profile.taskOne.id)
+    if profile.taskTwo:
+        currentTaskIDs.append(profile.taskTwo.id)
+    if profile.taskThree:
+        currentTaskIDs.append(profile.taskThree.id)
+
+    availableTasks = allTasks.exclude(pk__in=currentTaskIDs)
+    currentTasks = Task.objects.filter(id__in=currentTaskIDs)
+
+    return render(request, 'tasks/tasks.html', {'currentTasks': currentTasks,'availableTasks': availableTasks})
