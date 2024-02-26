d372b55cc7f2b4afb4ff63718823a38db7a14b73
Charlie Silver <cs1108@exeter.ac.uk>
2024-02-26 16:23:04 +0000

    Added Logo to Navbar

--- tasks/models.py
+++ tasks/models.py
@@ -7,9 +7,10 @@ class Task_Type(models.Model):
         return self.task_type
 
 class Task(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=200)
+    name = models.CharField(max_length=30, default='Name')
+    location = models.CharField(max_length=30, default='Location')
+    description = models.CharField(max_length=200, default='description')
     task_type = models.ForeignKey(Task_Type, on_delete=models.CASCADE)
-    answer = models.CharField(max_length=300)
+    answer = models.CharField(max_length=300, default='Answer')
     def __str__(self):
         return self.name
