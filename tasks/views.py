8a36d2d325e74b9cd5457f5b62dc0ab380cb9d97
lh880 <114520682+lh880@users.noreply.github.com>
2024-02-26 16:24:38 +0000

    started to add comments

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,22 @@
+# imports all libraries needed
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Task, Task_Type
 from .forms import FindTask, CompleteTask
 
+# this view will display all the tasks which are currently active, and which ones the user has available
 def task_view(request):
+    # lists all the tasks that are currently held in the database
     allTasks = Task.objects.all()
+    # gets the instance of the user, and the subsequent profile attached to it
     user = request.user
     profile = user.profile
 
+    # creates an array of all the ids of the tasks that the user currently has active
     currentTaskIDs=[]
 
+    # checks each task attribute in the profile class and adds the id to the array if a task is present
     if profile.taskOne:
         currentTaskIDs.append(profile.taskOne.id)
     if profile.taskTwo:
@@ -18,24 +24,33 @@ def task_view(request):
     if profile.taskThree:
         currentTaskIDs.append(profile.taskThree.id)
 
+    # creates a list of all tasks, excluding the current tasks
     availableTasks = allTasks.exclude(pk__in=currentTaskIDs)
+    # creates a list of all the current tasks based on the ids
     currentTasks = Task.objects.filter(id__in=currentTaskIDs)
 
+    # returns all the data to the page for it to be used
     return render(request, 'tasks/tasks.html', {'currentTasks': currentTasks,'availableTasks': availableTasks, 'profile': profile})
 
+# this view adds tasks to the profile if there is space available
 def add_task(request):
     if request.method == 'POST':
+        # uses the FindTask form in order to get the id of the task to add
         form = FindTask(request.POST)
         if form.is_valid():
+            # gets the instance of the user, and the subsequent profile attached to it
             user = request.user
             profile = user.profile
+            # gets the id of the task from the form
             task_id = form.cleaned_data['id']
             
+            # ensures that the id returned is a valid task
             try:
                 task = Task.objects.get(id=task_id)
             except Task.DoesNotExist:
                 return redirect('task_view')
 
+            # finds the next available empty task slot and places the task in there
             if not profile.taskOne:
                 profile.taskOne = task
             elif not profile.taskTwo:
@@ -45,6 +60,8 @@ def add_task(request):
             else:
                 pass
 
+            # save the profile and then exits
+            
             profile.save()
             return redirect('task_view')
     else:
