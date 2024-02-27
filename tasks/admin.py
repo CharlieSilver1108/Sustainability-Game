26d6e37a2321103a04061e45bedb5be69ad762ee
liam buchanan <=>
2024-02-27 11:34:01 +0000

    add to navbar

--- tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Task, Task_Type, MultipleChoiceTask, PersonBasedCode
-
-admin.site.register(Task)
-admin.site.register(Task_Type)
-admin.site.register(MultipleChoiceTask)
-admin.site.register(PersonBasedCode)
