fd344017e0d7435316a6c9c3eae8c597f21b9427
Luke Hales <lh880@exeter.ac.uk>
2024-02-29 00:13:46 +0000

    updating to merge with main

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,17 +2,12 @@ from django.urls import path
 from . import views
 from .views import add_task, task_view, remove_task, complete_task
 
-# ------- CODING BY LUKE HALES -------
-
 # adds all of the views to the urlpatterns array
 urlpatterns = [
     path('task_view', views.task_view, name='task_view'),
     path('add_task', views.add_task, name='add_task'),    
     path('remove_task', views.remove_task, name='remove_task'),
     path('complete_task', views.complete_task, name='complete_task'),
-
-# ------- END -------
-
     path('create_task_page', views.create_task_page, name='create_task_page'),
     path('create_task', views.create_task, name='create_task'),
     path('qr_explain', views.qr_explain, name='qr_explain'),
