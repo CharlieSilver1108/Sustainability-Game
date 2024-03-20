640a923da4e313abf3f8e2cb5c97faa8b0f908cf
greggoursaud <greggoursaud@outlook.com>
2024-03-20 19:28:59 +0000

    Team Scores Leaderboard

--- Sustain/views.py
+++ Sustain/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from django.http import HttpResponse
 from html.parser import HTMLParser
 from members.models import *
@@ -21,6 +22,8 @@ def how_to_play(request):
     return render(request, 'Sustain/how_to_play.html', {})
 
 
+from django.db.models import Sum
+
 def leaderboard(request):
     all_profiles = Profile.objects.order_by('-points')
     top_ten = all_profiles[:10]
@@ -32,10 +35,39 @@ def leaderboard(request):
             profile.highest_position = position
             profile.save()
 
+    # Calculate team points
+    team_points = Profile.objects.values('team__name').annotate(total_points=Sum('points'))
+
+    # Initialize team points
+    eco_pioneers_points = 0
+    green_guardians_points = 0
+    recycle_rangers_points = 0
+
+    # Assign team points
+    for team in team_points:
+        if team['team__name'] == 'Eco Pioneers':
+            eco_pioneers_points = team['total_points']
+        elif team['team__name'] == 'Green Guardians':
+            green_guardians_points = team['total_points']
+        elif team['team__name'] == 'Recycle Rangers':
+            recycle_rangers_points = team['total_points']
+
     if request.user.is_authenticated:
         current_user = Profile.objects.get(user=request.user)
         current_user_position = list(all_profiles).index(current_user) + 1
-        return render(request, 'Sustain/leaderboard.html', {'top_ten':top_ten, 'current_user_position':current_user_position})
+        return render(request, 'Sustain/leaderboard.html', {
+            'top_ten': top_ten, 
+            'current_user_position': current_user_position,
+            'eco_pioneers_points': eco_pioneers_points,
+            'green_guardians_points': green_guardians_points,
+            'recycle_rangers_points': recycle_rangers_points,
+        })
     else:
-        return render(request, 'Sustain/leaderboard.html', {'top_ten':top_ten})
+        return render(request, 'Sustain/leaderboard.html', {
+            'top_ten': top_ten,
+            'eco_pioneers_points': eco_pioneers_points,
+            'green_guardians_points': green_guardians_points,
+            'recycle_rangers_points': recycle_rangers_points,
+        })
+
 # ------- Charlie END -------
